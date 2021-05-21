f9a0abc0b2e60ef20a3c21fb15ed94cfa63e9a79
Marco Espinosa <marco@tinkin.one>
2021-05-21 13:30:10 -0500

    Fix broken tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  "testPathIgnorePatterns": [
-    "/node_modules/",
-    ".tmp",
-    ".cache"
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '.cache'
   ],
-  "testEnvironment": "node"
-};
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  globalSetup: './test-utils/globalSetup',
+  globalTeardown: './test-utils/globalTeardown',
+  setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
+  verbose: false,
+}
--- test/app.test.js
+++ test/app.test.js
@@ -6,20 +6,6 @@ beforeAll(async done => {
   done();
 });
 
-afterAll(async done => {
-  const dbSettings = strapi.config.get('database.connections.default.settings');
-
-  //delete test database after all tests
-  console.log(d)
-  if (dbSettings && dbSettings.filename) {
-    const tmpDbFile = `${__dirname}/../${dbSettings.filename}`;
-    if (fs.existsSync(tmpDbFile)) {
-      fs.unlinkSync(tmpDbFile);
-    }
-  }
-  done();
-});
-
 describe('Test for strapi app', () => {
   it('strapi is defined', async done => {
     expect(strapi).toBeDefined();
